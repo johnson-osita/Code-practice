87ff4624c73190ee19aae8509e46ac6bd6cb089a
johnson <117835911+johnson-osita@users.noreply.github.com>
2022-11-09 17:09:51 +0100

    init

---
+++ germaine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace germaine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a word");
+            string word = Console.ReadLine();
+            if (word.Length > 5)
+            {
+                var LastFourWord = word.Substring(word.Length - 4);
+                var otherWord = word.Substring(0, word.Length - 4);
+
+                var hash = "";
+                foreach (char letter in otherWord)
+                {
+                    hash += letter.ToString().Replace(letter, '#');
+
+                }
+                var newWord = hash + LastFourWord;
+                Console.WriteLine(newWord);
+            }
+            else
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
