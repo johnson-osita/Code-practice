04f762847ab2ab26edd0ecf511abe1c4d07c7e9d
johnson <117835911+johnson-osita@users.noreply.github.com>
2022-11-09 17:10:39 +0100

    init

---
+++ germaine2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace germaine2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter : year");
+            string year = Console.ReadLine();
+
+            Console.WriteLine("Enter : month");
+            string month = Console.ReadLine();
+
+            Console.WriteLine("Enter : day");
+            string day = Console.ReadLine();
+
+            string date = $"{year}, {month}, {int.Parse(day)+7}".ToString();
+
+
+            DateTime value;
+            if (DateTime.TryParse(date, out value))
+            {
+                DateTime date2 = DateTime.Parse(date);
+                Console.WriteLine(date2.ToString("F"));
+            }
+            else
+            {
+                Console.WriteLine("you entered an invalid date");
+            }
+
+        }
+    }
+}
---
+++ germaine2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("germaine2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("germaine2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f169b142-363f-4696-b30e-02b4258c6488")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
