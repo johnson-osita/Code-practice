0ce58029a61bd2dfc67ee8e961afe0c2c7da494e
johnson <117835911+johnson-osita@users.noreply.github.com>
2022-11-09 17:11:35 +0100

    init

---
+++ germaine3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace germaine3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a word");
+            string word = Console.ReadLine();
+
+
+            if (word.Any(letter => char.IsUpper(letter)))
+            { 
+                foreach (char letter in word)
+                {
+                    if (Char.IsUpper(letter))
+                    {
+                        Console.WriteLine(word.IndexOf(letter));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Word does not contain an uppercase latter");
+            }
+        }
+    }
+}
